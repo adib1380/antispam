01873604f3062af7ff598c684e871af2eae7d74a
adib1380 <adib.1380.nj@gmail.com>
2016-06-12 11:36:18 -0700

    Update mehrpouya.lua

--- plugins/mehrpouya.lua
+++ plugins/mehrpouya.lua
@@ -1,16 +1,16 @@
 do
 
 function run(msg, matches)
-  return " MeHrPoUyA Is KiNg OF bOtS 😘"
+  return " Adib Is KiNg OF bOtS 😘"
 end
 return {
   description = "Invite bot into a group chat", 
   usage = "!join [invite link]",
   patterns = {
-    "^مهرپویا$",
-    "^mehrpouya$",
-    "^MEHRPOUYA$",
-    "^Mehrpouya$",
+    "^ادیب$",
+    "^Adib$",
+    "^Adib$",
+    "^adib$",
   },
   run = run
 }
